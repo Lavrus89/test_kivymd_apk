a21bfc93317ae9548be7ea511858de5b31a33d18
Lavrus89 <85923747+Lavrus89@users.noreply.github.com>
2023-05-30 13:02:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
+
+class MyApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return (
+            MDScreen(
+                MDRectangleFlatButton(
+                    text="Кнопка",
+                    pos_hint={"center_x": 0.5, "center_y": 0.5}
+                )
+            )
+        )
+
+MyApp().run()
